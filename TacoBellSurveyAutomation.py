eeb658f3eb68314f47dec80529c3b0b8c628bdb9
Jared Myers <jarednm@bgsu.edu>
2021-06-06 22:39:12 -0400

    Next Button Function

--- TacoBellSurveyAutomation.py
+++ TacoBellSurveyAutomation.py
@@ -1,4 +1,10 @@
 from selenium import webdriver
+#for testing
+import time
+
+def NextButton():
+    Next1 = driver.find_element_by_xpath('//*[@id="NextButton"]')
+    Next1.click()
 
 driver = webdriver.Chrome()
 
@@ -19,10 +25,10 @@ start_button_click.click()
 # Screen 1 - Overall Satisfaction
 HighSatisfy1 = driver.find_element_by_xpath('//*[@id="FNSR001000"]/td[2]/span')
 HighSatisfy1.click()
-Next1 = driver.find_element_by_xpath('//*[@id="NextButton"]')
-Next1.click()
 
-# Screen 2 - Rate Satisfication Big Chunk
+NextButton()
+
+# Screen 2 - Rate Satisfication Big Chunk Completely Random Order
 SpeedService = driver.find_element_by_xpath('//*[@id="FNSR012000"]/td[2]/span')
 SpeedService.click()
 
@@ -32,7 +38,6 @@ HealthSafety.click()
 FriendlyTeam = driver.find_element_by_xpath('//*[@id="FNSR010000"]/td[2]/span')
 FriendlyTeam.click()
 
-# Might be missing accuracy?
 Accuracy = driver.find_element_by_xpath('//*[@id="FNSR008000"]/td[2]/span')
 Accuracy.click()
 
@@ -45,78 +50,60 @@ Exterior.click()
 PortionSize = driver.find_element_by_xpath('//*[@id="FNSR007000"]/td[2]/span')
 PortionSize.click()
 
-# Is this Identical to above? if so remove and reuse!
-Next2 = driver.find_element_by_xpath('//*[@id="NextButton"]')
-Next2.click()
+# NEED TASTE?? It still completes but it might add it due to automation?
+time.sleep(30)
+NextButton()
 
-# Screen 3 DOES THIS EXIST NOW?
-Accuracy2 = driver.find_element_by_xpath('//*[@id="FNSR008000"]/td[2]/span')
-Accuracy2.click()
-
-Next1 = driver.find_element_by_xpath('//*[@id="NextButton"]')
-Next1.click()
-
-# Screen 4 - Problem In Visit
+# Screen 3 - Problem In Visit
 Problem = driver.find_element_by_xpath('//*[@id="FNSR024000"]/td[3]/span')
 Problem.click()
 
-# Does this work?
-Next1 = driver.find_element_by_xpath('//*[@id="NextButton"]')
-Next1.click()
+NextButton()
 
-# Screen #5 - You said You were highly satisfied
-StockResponse1 = "Food was amazing and the speed was incredible"
+# Screen 4 - You said You were highly satisfied
+StockResponse1 = "Food was amazing and the speed was incredible!"
 TextBox1 = driver.find_element_by_xpath('//*[@id="S081000"]')
 TextBox1.send_keys(StockResponse1)
 
-# Does this work?
-Next1 = driver.find_element_by_xpath('//*[@id="NextButton"]')
-Next1.click()
+time.sleep(15)
+NextButton()
 
-# Screen #6 - Reconize Team Member
+# Screen 4 - Reconize Team Member? Yes
 ReconizeMember = driver.find_element_by_xpath('//*[@id="FNSR030000"]/td[2]/span')
+ReconizeMember.click()
+NextButton()
 
-# Does this work?
-Next1 = driver.find_element_by_xpath('//*[@id="NextButton"]')
-Next1.click()
-
-# Screen 7 - Name Drop Time
+# Screen 5 - Name Drop Time
 # Team Member Name
 TeamMemberName = driver.find_element_by_xpath('//*[@id="S081001"]')
 TeamMemberName.send_keys(cashier_name)
 
-TeamMemberDescription = driver.find_element_by_xpath('//*[@id="S081001"]')
-FillResponse = cashier_name + " did an excellent job taking care of myself and other customers in the line. Exception work!"
+TeamMemberDescription = driver.find_element_by_xpath('//*[@id="S081002"]')
+FillResponse = cashier_name + " did an excellent job taking care of myself and other customers in the line. Exceptional work!"
 TeamMemberDescription.send_keys(FillResponse)
 
-# Does this work?
-Next1 = driver.find_element_by_xpath('//*[@id="NextButton"]')
-Next1.click()
+time.sleep(15)
+NextButton()
 
-# Screen 8 - Hard or Soft Shell?
+# Screen 6 - Hard or Soft Shell?
 HardSoft = driver.find_element_by_xpath('//*[@id="FNSR031000"]/td[3]/span')
 HardSoft.click()
 
-# Does this work?
-Next1 = driver.find_element_by_xpath('//*[@id="NextButton"]')
-Next1.click()
+NextButton()
 
-# Screen 9 - Health Saftey Extended Response
+# Screen 7 - Health Saftey Extended Response
 HealthySafetyER = driver.find_element_by_xpath('//*[@id="S000124"]')
 FillResponse2 = cashier_name + " made me feel safe. All interactions followed strict guidelines regarding saftey!"
 HealthySafetyER.send_keys(FillResponse2)
 
-# Does this work?
-Next1 = driver.find_element_by_xpath('//*[@id="NextButton"]')
-Next1.click()
+NextButton()
 
 # Screen 8 - Enter Sweepstakes?
 Sweepstakes = driver.find_element_by_xpath('//*[@id="FNSR046000"]/td[3]/span')
 Sweepstakes.click()
 
-# Does this work?
-Next1 = driver.find_element_by_xpath('//*[@id="NextButton"]')
-Next1.click()
+# Does this work? commented out for obvi reasons
+#NextButton()
 
 ##############################################
 #invalid_code = driver.find_element_by_xpath('//*[@id="surveyQuestions"]/div[1]')
