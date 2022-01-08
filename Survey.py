133e50c1a88e93c5ab8f5d9faf2409529ede1d6a
Jared Myers <jarednm@bgsu.edu>
2022-01-08 13:19:49 -0500

    Fixed Bugs; Added Webdriver

--- Survey.py
+++ Survey.py
@@ -17,12 +17,12 @@ try:
     driver.minimize_window()
 
     # Finds Entry Box and starts typing
-    SixteenDigitCode = input("Please enter the 16 digit code from your survey: ")
-    CashierName = input("Please enter the name listed team member: ")
+    #SixteenDigitCode = input("Please enter the 16 digit code from your survey: ")
+    #CashierName = input("Please enter the name listed team member: ")
     
     #For Dev
-    SixteenDigitCode = "1525803122870026"
-    CashierName = "Justin"
+    SixteenDigitCode = "8041903919073402"
+    CashierName = "Alexis"
 
     # Minimizes the window for easy of use
     driver.maximize_window()
@@ -36,7 +36,7 @@ try:
     # If the name includes non-letter characters it fails
     # Thus, spaces/more than one name ruins the fun
     if(CashierName.isalpha() == False):
-       print("Error: Name Contains Numbers/Symbols")
+       print("Error: Name Contains Numbers/Spaces/Symbols")
        time.sleep(15)
        sys.exit()
 
@@ -52,65 +52,68 @@ try:
     StartButton.click()
 
     # Screen 1 - Overall Satisfaction
-    HighSatisfy = driver.find_element_by_xpath('//*[@id="FNSR001000"]/td[2]/span')
+    HighSatisfy = driver.find_element_by_xpath('//*[@id="FNSR001000"]/td[1]/span')
     HighSatisfy.click()
     NextButton()
 
     # Screen 2 - Rate Satisfication | Big Chunk | Completely Random Order
     try:
-        SpeedService = driver.find_element_by_xpath('//*[@id="FNSR012000"]/td[2]/span')
+        SpeedService = driver.find_element_by_xpath('//*[@id="FNSR012000"]/td[1]/span')
         SpeedService.click()
+
     except:
         print("Option Not Present; Skipped")
 
     try:
-        HealthSafety = driver.find_element_by_xpath('//*[@id="FNSR000121"]/td[2]/span')
+        HealthSafety = driver.find_element_by_xpath('//*[@id="FNSR000121"]/td[1]/span')
         HealthSafety.click()
     except:
         print("Option Not Present; Skipped")
 
     try:
-        FriendlyTeam = driver.find_element_by_xpath('//*[@id="FNSR010000"]/td[2]/span')
+        FriendlyTeam = driver.find_element_by_xpath('//*[@id="FNSR010000"]/td[1]/span')
         FriendlyTeam.click()
     except:
         print("Option Not Present; Skipped")
 
     try:
-        Accuracy = driver.find_element_by_xpath('//*[@id="FNSR008000"]/td[2]/span')
+        Accuracy = driver.find_element_by_xpath('//*[@id="FNSR008000"]/td[1]/span')
         Accuracy.click()
     except:
         print("Option Not Present; Skipped")
 
     try:
-        Appearance = driver.find_element_by_xpath('//*[@id="FNSR011000"]/td[2]/span')
+        Appearance = driver.find_element_by_xpath('//*[@id="FNSR011000"]/td[1]/span')
         Appearance.click()
     except:
         print("Option Not Present; Skipped")
 
     try:
-        Exterior = driver.find_element_by_xpath('//*[@id="FNSR014000"]/td[2]/span')
+        Exterior = driver.find_element_by_xpath('//*[@id="FNSR014000"]/td[1]/span')
         Exterior.click()
     except:
         print("Option Not Present; Skipped")
 
     try:
-        PortionSize = driver.find_element_by_xpath('//*[@id="FNSR007000"]/td[2]/span')
+        PortionSize = driver.find_element_by_xpath('//*[@id="FNSR007000"]/td[1]/span')
         PortionSize.click()
     except:
         print("Option Not Present; Skipped")
 
     try:
-        Taste = driver.find_element_by_xpath('//*[@id="FNSR005000"]/td[2]/span')
+        Taste = driver.find_element_by_xpath('//*[@id="FNSR005000"]/td[1]/span')
         Taste.click()
     except:
             print("Option Not Present; Skipped")
 
     NextButton()
 
-    # Screen 3 - Problem In Visit
-    Problem = driver.find_element_by_xpath('//*[@id="FNSR024000"]/td[3]/span')
+    # Screen 3 - Problem In Visit (No)
+    Problem = driver.find_element_by_xpath('//*[@id="FNSR024000"]/td[2]/span')
     Problem.click()
     NextButton()
+
+    
 
     # Screen 4 - You said You were highly satisfied
     # Pick a Stock Response
@@ -118,6 +121,7 @@ try:
     StockResponse0 = "Food was amazing and the speed was incredible!"
     StockResponse1 = "Food tasted good."
     StockResponse2 = "Did not run into a single shortcoming!"
+    
     choice = random.randint(0,2)
 
     if (choice == 0):
@@ -126,14 +130,18 @@ try:
         FinalResponse = StockResponse1
     else:
         FinalResponse = StockResponse2
+    print(FinalResponse)
 
     TextBox1 = driver.find_element_by_xpath('//*[@id="S081000"]')
+    TextBox1.click()
     TextBox1.send_keys(FinalResponse)
+
     NextButton()
 
     # Screen 4 - Reconize Team Member? Yes
-    ReconizeMember = driver.find_element_by_xpath('//*[@id="FNSR030000"]/td[2]/span')
+    ReconizeMember = driver.find_element_by_xpath('//*[@id="FNSR030000"]/td[1]/span')
     ReconizeMember.click()
+
     NextButton()
 
     # Screen 5 - Name Drop Time
@@ -160,18 +168,21 @@ try:
     NextButton()
 
     # Screen 6 - Hard or Soft Shell?
-    HardSoft = driver.find_element_by_xpath('//*[@id="FNSR031000"]/td[3]/span')
-    HardSoft.click()
+    HardShell = driver.find_element_by_xpath('//*[@id="FNSR031000"]/td[2]/span')
+    HardShell.click()
+    CrispyChickenTaco = driver.find_element_by_xpath('//*[@id="FNSR000127"]/td[2]/span')
+    CrispyChickenTaco.click()
+
     NextButton()
 
     # Screen 7 - Health Safety Extended Response
     HealthySafetyER = driver.find_element_by_xpath('//*[@id="S000124"]')
-    FillResponse2 = CashierName + " made me feel safe. All interactions followed strict guidelines regarding saftey!"
+    FillResponse2 = CashierName + " made me feel safe. All interactions followed decent guidelines regarding saftey!"
     HealthySafetyER.send_keys(FillResponse2)
     NextButton()
 
-    # Screen 8 - Enter Sweepstakes?
-    Sweepstakes = driver.find_element_by_xpath('//*[@id="FNSR046000"]/td[3]/span')
+    # Screen 8 - Enter Sweepstakes? (No)
+    Sweepstakes = driver.find_element_by_xpath('//*[@id="FNSR046000"]/td[2]/span')
     Sweepstakes.click()
 
     # Final Submission
